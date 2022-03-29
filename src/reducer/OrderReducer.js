e2dfe849952d819b8f8c973bfb8788958ff332b7
Rutvik Umak <rutvikumak@gmail.com>
2022-03-29 15:54:39 +0530

    added missing file

---
+++ src/reducer/OrderReducer.js
@@ -0,0 +1,36 @@
+import { ACTION_TYPE } from "../utils";
+
+export const orderState = {
+  address: [],
+  order: [],
+  priceDetails: {
+    price: 0,
+    discount: 0,
+    coupon: 0,
+    total: 0,
+  },
+};
+
+export const orderReducer = (state, action) => {
+  switch (action.type) {
+    case ACTION_TYPE.PRICE_DETAILS:
+      const { priceOnMRP, discountOnMRP, couponApplied } = action.payload;
+      console.log({
+        ...state,
+        priceDetails: {
+          ...state.priceDetails,
+          price: price,
+          discount: discount,
+        },
+      });
+      return {
+        ...state,
+        priceDetails: {
+          price: priceOnMRP,
+          discount: discountOnMRP,
+        },
+      };
+    case "default":
+      return state;
+  }
+};
